d8e57770cd29b5ab501c298821a19ac02ec22164
kevin7520 <kevin7520@hotmail.com>
2023-05-13 13:13:33 -0500

    Creación de componentes

---
+++ src/app/Components/Header/Header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-Header',
+  templateUrl: './Header.component.html',
+  styleUrls: ['./Header.component.Scss']
+})
+export class HeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+/* Componentes creados */
+import { HomeComponent } from './View/Home/Home.component';
+import { HeaderComponent } from './Components/Header/Header.component';
+import { FooterComponent } from './Components/Footer/Footer.component';
+
+/* Importaciones de prime ng */
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
